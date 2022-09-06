79e2e41c9f5f42dad1a7351e031a343c785a8a7c
Alan2Romero <filliad123@gmail.com>
2022-09-06 15:53:15 -0300

    Ejercicio para probar Print

---
+++ Ejercicio218.java
@@ -0,0 +1,21 @@
+/*
+
+ * Nombre de la clase: Clase de la semana del 22
+ * Información de la versión: 
+ * Fecha: 4/9/2022
+ * Enunciado: Escriba una aplicación que muestre un cuadro, un óvalo, una flecha y un diamante usando asteriscos (*)
+ */
+
+public class Ejercicio218 {
+    public static void main(String[] args) {
+        System.out.println("*********         ***            *              *");
+        System.out.println("*       *      *       *        ***           *   *");
+        System.out.println("*       *     *         *      *****         *     *");
+        System.out.println("*       *     *         *        *          *       * ");
+        System.out.println("*       *     *         *        *         *         *");
+        System.out.println("*       *     *         *        *          *       *");
+        System.out.println("*       *     *         *        *           *     *");
+        System.out.println("*       *      *       *         *             *  *");
+        System.out.println("*********         ***            *              *");
+    } 
+}
